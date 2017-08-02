00dd5f1276570ea9193427072a25ac3fdde53eed
Guo Xiang Tan <tgx_world@hotmail.com>
2017-08-02 15:56:11 +0900

    Fix rubocop offenses.

--- lib/math_renderer.rb
+++ lib/math_renderer.rb
@@ -1,6 +1,6 @@
 # the idea here is to support server rendering
 # there is a node example and we could inject this in a post processing job
 class DiscourseMath::MathRenderer
-  def self.render(mathjax, options={})
+  def self.render(mathjax, options = {})
   end
 end
