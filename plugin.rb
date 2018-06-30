ff6d2fc1d76841aeeaf81ead5dc1c4c690950229
Joshua Rosenfeld <jomaxro@discourse.org>
2018-06-30 17:18:39 -0400

    Add URL to plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,5 +2,6 @@
 # about: Official mathjax plugin for Discourse
 # version: 0.9
 # authors: Sam Saffron (sam)
+# url: https://github.com/discourse/discourse-math
 
 enabled_site_setting :discourse_math_enabled
