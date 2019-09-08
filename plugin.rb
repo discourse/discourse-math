496cbb94b2b8d90579df67af990ba19ef36b8fdc
Joe <johani@discourse.org>
2019-09-08 17:49:56 +0800

    adds responsive styles

--- plugin.rb
+++ plugin.rb
@@ -6,4 +6,6 @@
 # authors: Sam Saffron (sam)
 # url: https://github.com/discourse/discourse-math
 
+register_asset "stylesheets/common/discourse-math.scss"
+
 enabled_site_setting :discourse_math_enabled
